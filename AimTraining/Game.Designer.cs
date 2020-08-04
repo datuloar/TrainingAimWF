23d47ba9321465f451253e52f3607ddef56add6f
datuloar <teeky@bk.ru>
2020-08-04 17:00:37 +0500

    Simple Aim Training on C#

---
+++ AimTraining/Game.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace AimTraining
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.UserScoreLabel = new System.Windows.Forms.Label();
+            this.Mode = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // UserScoreLabel
+            // 
+            this.UserScoreLabel.AutoSize = true;
+            this.UserScoreLabel.BackColor = System.Drawing.Color.FloralWhite;
+            this.UserScoreLabel.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserScoreLabel.Location = new System.Drawing.Point(813, 23);
+            this.UserScoreLabel.Name = "UserScoreLabel";
+            this.UserScoreLabel.Size = new System.Drawing.Size(21, 23);
+            this.UserScoreLabel.TabIndex = 0;
+            this.UserScoreLabel.Text = "0";
+            // 
+            // Mode
+            // 
+            this.Mode.AutoSize = true;
+            this.Mode.BackColor = System.Drawing.Color.FloralWhite;
+            this.Mode.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Mode.Location = new System.Drawing.Point(12, 23);
+            this.Mode.Name = "Mode";
+            this.Mode.Size = new System.Drawing.Size(114, 23);
+            this.Mode.TabIndex = 1;
+            this.Mode.Text = "Easy Level";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SandyBrown;
+            this.BackgroundImage = global::AimTraining.Resource.Background;
+            this.ClientSize = new System.Drawing.Size(934, 576);
+            this.Controls.Add(this.Mode);
+            this.Controls.Add(this.UserScoreLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Training Aim";
+            this.TopMost = true;
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label UserScoreLabel;
+        private System.Windows.Forms.Label Mode;
+    }
+}
+
---
+++ AimTraining/Game.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace AimTraining
+{
+    public partial class Game : Form
+    {
+        private Bitmap _crosshairTexture = Resource.Crosshair;
+        private Bitmap _targetTexture = Resource.BlackAim;
+
+        private Point _targetPosition = new Point(450, 450);
+        private Point _direction = Point.Empty;
+
+        private bool _middleLevel => _score > 200 && _score < 900;
+        private bool _hardLevel => _score > 900 && _score < 4000;
+        private bool _superHardLevel => _score > 4000;
+        private bool _isGameEnd => _score > 5000;
+
+        private uint _score;
+
+        public Game()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint, true);
+
+            UpdateStyles();
+            Cursor.Hide();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (!_isGameEnd)
+            {
+                Graphics graphics = e.Graphics;
+
+                _targetPosition.X += _direction.X * TargetSpeed();
+                _targetPosition.Y += _direction.Y * TargetSpeed();
+
+                if (_targetPosition.X < 0 || _targetPosition.X > 500)
+                {
+                    _direction.X *= -1;
+                }
+
+                if (_targetPosition.Y < 0 || _targetPosition.Y > 500)
+                {
+                    _direction.Y *= -1;
+                }
+
+                var localPosition = this.PointToClient(Cursor.Position);
+                var crosshairRectangle = new Rectangle(localPosition.X - 10, localPosition.Y - 10, 20, 20);
+                var targetRectangle = new Rectangle(_targetPosition.X, _targetPosition.Y, 50, 50);
+
+                Point between = new Point(localPosition.X - _targetPosition.X, localPosition.Y - _targetPosition.Y);
+                float distance = (float)Math.Sqrt((between.X * between.X) + (between.Y * between.Y));
+
+                if (distance < 50)
+                {
+                    AddScore(5);
+                }
+                else
+                {
+                    if (_score > 0)
+                    {
+                        RemoveScore(1);
+                    }
+                }
+
+                graphics.DrawImage(_targetTexture, targetRectangle);
+                graphics.DrawImage(_crosshairTexture, crosshairRectangle);
+            }
+            else
+            {
+                Mode.Text = "GAME END U HAVE OVER 5000 POINTS!";
+            }
+        }
+
+        private void AddScore(uint score)
+        {
+            _score += score;
+            UserScoreLabel.Text = _score.ToString();
+        }
+
+        private void RemoveScore(uint score)
+        {
+            _score -= score;
+            UserScoreLabel.Text = _score.ToString();
+        }
+
+        private int TargetSpeed()
+        {
+            if (_middleLevel)
+            {
+                Mode.Text = "Middle Level";
+                return 9;
+            }
+            else if (_hardLevel)
+            {
+                Mode.Text = "Hard Level";
+                return 12;
+            }
+            else if (_superHardLevel)
+            {
+                Mode.Text = "Super-Hard Level";
+                return 20;
+            }
+
+            Mode.Text = "Easy Level";
+            return 5;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!_isGameEnd) Refresh();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();
+
+            timer2.Interval = random.Next(100, 1000);
+
+            _direction.X = random.Next(-1, 2);
+            _direction.Y = random.Next(-1, 2);
+        }
+    }
+}
